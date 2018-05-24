002bbd9a79850523d1ef3403063c5f3d34c63c03
jedineeson <1810027@bart.ca>
2018-05-24 13:10:14 -0400

    Nouveau Artwork et meilleur mouvement

--- Projet/Assets/Scripts/PlayerController.cs
+++ Projet/Assets/Scripts/PlayerController.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Quaternion m_PlayerRot;
-    private Vector3 m_TargetPosition = new Vector3();
-
     public Rigidbody m_Player1;
     public Rigidbody m_Player2;
-    public float m_Speed = 10f;
-    public Vector3 m_MoveDir = new Vector3();
+    public float m_PlayerSpeed = 10f;
+
+    public float m_DodgeTimer = 0f;
+    public float m_DodgeDuration = 0f;
+
+    private bool m_DodgeIsActive = false;
+    private Quaternion m_PlayerRot;
+    private Vector3 m_TargetPosition = new Vector3();
+    private Vector3 m_MoveDir = new Vector3();
 
     private void Start()
     {
-      
+
     }
 
     private void Update()
@@ -22,7 +26,22 @@ public class PlayerController : MonoBehaviour
         m_TargetPosition = m_Player2.transform.position;
         this.transform.LookAt(m_TargetPosition);
 
-        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        //while (m_DodgeIsActive)
+        //{
+        //    m_DodgeDuration = 0.2f;
+        //    m_PlayerSpeed = 20f;
+        //    m_MoveDir = -transform.right;
+        //}
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            m_MoveDir = -transform.forward;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            m_MoveDir = transform.forward;
+        }
+        else if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
         {
             m_MoveDir = -transform.right;
         }
@@ -32,13 +51,39 @@ public class PlayerController : MonoBehaviour
         }
         else
         {
+            m_PlayerSpeed = 10f;
             m_MoveDir = Vector3.zero;
         }
+
+        //if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        //{
+        //    m_DodgeTimer = 0.5f;
+        //}
+        //if ((Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) && m_DodgeTimer > 0f)
+        //{
+        //    m_DodgeIsActive = true;
+        //}
+
+        //if (m_DodgeTimer > 0)
+        //{
+        //    m_DodgeTimer -= 0.1f;
+        //}
+
+
+        //if (m_DodgeDuration > 0)
+        //{
+        //    m_DodgeDuration -= 0.1f;
+        //    if(m_DodgeDuration <= 0)
+        //    {
+        //        m_DodgeIsActive = false;
+        //    }
+        //}
+
     }
 
     private void FixedUpdate()
     {
-        m_MoveDir *= m_Speed;
+        m_MoveDir *= m_PlayerSpeed;
         m_MoveDir.y = m_Player1.velocity.y;
         m_Player1.velocity = m_MoveDir;
     }
